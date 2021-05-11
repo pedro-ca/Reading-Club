835d1e4f686f593b4c9a0250bc9d95dab438fda9
Bleyk <50923316+pedro-ca@users.noreply.github.com>
2021-05-11 17:31:37 -0300

    Added Control classes

---
+++ ReadingClub/ReadingClub.ConsoleApp/Control/ControllerMagazine.cs
@@ -0,0 +1,12 @@
+﻿using ReadingClub.ConsoleApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadingClub.ConsoleApp.Control
+{
+    class ControllerMagazine : ControllerBase
+    {
+        List<Magazine> magazines;
+    }
+}
---
+++ ReadingClub/ReadingClub.ConsoleApp/Control/ControllerStorageBox.cs
@@ -0,0 +1,12 @@
+﻿using ReadingClub.ConsoleApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadingClub.ConsoleApp.Control
+{
+    class ControllerStorageBox : ControllerBase
+    {
+        List<StorageBox> storageBoxes;
+    }
+}
